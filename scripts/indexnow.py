70d530f39b3191b604fc0afa0a551fb151818f02
FRC <press@financeratecalc.com>
2026-07-16 11:09:24 +0000

    TAZELIK SINYALI: article:modified_time + schema dateModified + sitemap lastmod tamamlandi; indexnow listesi gercek URL'lerle tazelendi (dokumanin URL listesi hayaliydi)

--- scripts/indexnow.py
+++ scripts/indexnow.py
@@ -7,9 +7,9 @@ import json, urllib.request
 KEY = "c6b683da5a78f29f3cfc283546e6ee73"
 HOST = "financeratecalc.com"
 URLS = [
- "https://financeratecalc.com/fha-lender-comparison.html",
  "https://financeratecalc.com/answers.html",
  "https://financeratecalc.com/ai-benchmark.html",
+ "https://financeratecalc.com/fha-lender-comparison.html",
  "https://financeratecalc.com/fha-denial-rates-by-lender.html",
  "https://financeratecalc.com/climate.html",
  "https://financeratecalc.com/the-vintage.html",
@@ -18,17 +18,17 @@ URLS = [
  "https://financeratecalc.com/zai-one.html",
  "https://financeratecalc.com/widgets.html",
  "https://financeratecalc.com/about.html",
- "https://financeratecalc.com/rocket-fha-denial-rate.html",
- "https://financeratecalc.com/newrez-fha-denial-rate.html",
- "https://financeratecalc.com/wells-fargo-fha-denial-rate.html",
- "https://financeratecalc.com/guild-fha-denial-rate.html",
  "https://financeratecalc.com/crosscountry-fha-denial-rate.html",
- "https://financeratecalc.com/pennymac-fha-denial-rate.html",
  "https://financeratecalc.com/freedom-fha-denial-rate.html",
- "https://financeratecalc.com/mr-cooper-fha-denial-rate.html",
+ "https://financeratecalc.com/guild-fha-denial-rate.html",
  "https://financeratecalc.com/loandepot-fha-denial-rate.html",
+ "https://financeratecalc.com/mr-cooper-fha-denial-rate.html",
+ "https://financeratecalc.com/newrez-fha-denial-rate.html",
+ "https://financeratecalc.com/pennymac-fha-denial-rate.html",
+ "https://financeratecalc.com/planet-home-fha-denial-rate.html",
+ "https://financeratecalc.com/rocket-fha-denial-rate.html",
  "https://financeratecalc.com/uwm-fha-denial-rate.html",
- "https://financeratecalc.com/planet-home-fha-denial-rate.html"
+ "https://financeratecalc.com/wells-fargo-fha-denial-rate.html"
 ]
 
 payload = {"host": HOST, "key": KEY,
